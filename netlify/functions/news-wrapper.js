1785436524ff4f663e3d7963ae9478f27bf4d7bb
Charles <charles@example.com>
2026-05-21 01:06:16 +0800

    Rename dashboard.html to index.html, use relative PWA paths, remove serverless functions

--- netlify/functions/news-wrapper.js
+++
@@ -1,3 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('./news');
-exports.handler = serverless(app);
--- sw-dash.js
+++ sw-dash.js
@@ -1,6 +1,6 @@
 const CACHE = 'global-news-v3';
 const ASSETS = [
-  './dashboard.html',
+  './index.html',
   './manifest_dash.json',
   './icon-192.png',
   './icon-512.png',
